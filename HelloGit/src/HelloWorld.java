64013ed5c063ef407da323603b8fce6b857bee28
Alin <wildpeanut@msn.com>
2014-07-23 09:08:29 +0800

    initial commit

---
+++ HelloGit/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * 程式功能: <br/>
+ * 程式日期: $Date$<br/>
+ * 程式版本: $Id$<br/>
+ * 上版人員: $Author$<br/>
+ * 程式備註: <br/>
+ */
+public class HelloWorld {
+
+	public static void main(String args[]){
+		System.out.println("Hello World!!");
+	}
+}
